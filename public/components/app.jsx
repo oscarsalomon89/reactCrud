5c5f42374fc71694fa673bf0409ef8deb17edb97
oscarsalomon89 <oscar.salomon89@gmail.com>
2017-03-13 09:26:13 -0300

    Se agrega redux
    se instala redux-react

--- public/components/app.jsx
+++ public/components/app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+//import store from './store';
 import ChatBox from './chatBox';
 
 ReactDOM.render(
-  <ChatBox />,
+  <Provider>
+    <ChatBox />
+  </Provider>,
   document.getElementById('root')
 );
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+//import store from './store';
 import ChatBox from './chatBox';
 
-ReactDOM.render(React.createElement(ChatBox, null), document.getElementById('root'));
+ReactDOM.render(React.createElement(
+  Provider,
+  null,
+  React.createElement(ChatBox, null)
+), document.getElementById('root'));
